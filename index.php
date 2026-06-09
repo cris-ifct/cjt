547571e7bbdc6cf9470af9f1c3b13a3b83601709
aegolius <cris.ifct0109@gmail.com>
2026-06-09 16:08:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/images/favicon-cv.jpg" rel="icon" type="image/x-icon" />
+    <link href="/styles/csjt.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sono:wght@200..800&display=swap" rel="stylesheet">
+
+    <title>Cristian Jara Toro -> CV | Resume</title>
+
+</head>
+
+<body class="body-index">
+
+<div class="media-invisible">
+    <p class="invisible">Content not available for screens < to 1024px</p>
+</div>
+
+<div class="media-visible">
+    <p class="index-nombre">Cristian Jara Toro</p>
+    <p class="index-cv">cv | resume</p>
+
+<div>
+    <div class="index-nav">
+        <a class="enter" href="ES/sobre-mi/sobre-mi">español</a>
+        <a class="enter" href="EN/about/about-me">english</a>
+</div>
+
+</div>
+
+</body>
+</html>
+
+   
